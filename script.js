76cfec8680e58a6ae0f25e67a4f267a80b2e61f6
KATYA SYLENE FONTENELLE PALHETA <109077065+KatyaPalheta@users.noreply.github.com>
2022-08-15 04:09:46 -0300

    Atualização do projeto calculadora
    Uso de imagens com transparencia, correção de cores

---
+++ script.js
@@ -0,0 +1,3 @@
+const operacao_histirico = document.querySelector("#operacao_histirico");
+const operacao_atual = document.querySelector("#operacao_atual");
+const botoes = document.querySelectorAll("#container_botoes button");
